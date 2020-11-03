82fd1e2999cec6a51b24496e18cd8520635a2da0
PavanKumar <73385866+PavanK949@users.noreply.github.com>
2020-11-04 00:11:37 +0530

    Email sender using python

---
+++ EmailSender.py
@@ -0,0 +1,42 @@
+# Program to send email using 'smtp' library.
+
+import smtplib
+from string import Template
+
+contactsFileName = "contacts.dat"
+bodyFileName = "BodyOfMail.txt"
+
+names = []
+emails = []
+
+with open(contactsFileName, 'r') as fContactsObj:
+	contacts = fContactsObj.readlines()
+fContactsObj.close()
+
+for contact in contacts:
+	names.append(contact.split(',')[0])
+	emails.append(contact.split(',')[1])
+
+with open(bodyFileName, 'r') as fBodyObj:
+	body = fBodyObj.read()
+	body = Template(body)
+fBodyObj.close()
+
+port = 587
+smtpServer = 'smtp.gmail.com'
+senderMailId = input("Enter your mail id: ")
+password = input("Enter your password: ")
+print("-" * 15)
+
+server = smtplib.SMTP(smtpServer, port)
+server.starttls()
+server.login(senderMailId, password)
+
+counter = 0
+for mailId in emails:
+	message = body.substitute(NAME = names[counter]).encode('utf-8')
+	server.sendmail(senderMailId, mailId, message)
+	print("Mail sent successfully.")
+	counter += 1
+
+server.quit()

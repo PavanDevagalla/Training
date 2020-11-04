7039e64d50f9e3ecf8c87480787a8ef6813ea82e
PavanKumar <73385866+PavanK949@users.noreply.github.com>
2020-11-04 19:43:03 +0530

    Program to get gray scale and increase the brightness or contrast level of given picture.

---
+++ ImageProcessing.py
@@ -0,0 +1,69 @@
+# Program to convert a color image to gray scale image.
+
+from PIL import Image, ImageFilter, ImageEnhance
+
+menuFileName = "Menu(1).cfg"
+inputPrompt = "Enter a number(1 - 100) to see the change in image: "
+
+image = Image.open('nature.jpg')
+
+with open(menuFileName, 'r') as fMenuObj:
+	menu = fMenuObj.read()
+fMenuObj.close()
+
+def convertColorImageToGrayScale():
+	enhancementOfImage = ImageEnhance.Color(image)
+	enhancementOfImage.enhance(0.0).show()
+
+def blur():
+	blurLevel = input(inputPrompt)
+	validateInput(blurLevel)
+	blurLevel = int(blurLevel)
+	blurLevel = (blurLevel * 20) / 100
+	blurredImage = image.filter(ImageFilter.GaussianBlur(radius = blurLevel))
+	blurredImage.show()
+
+def brightness():
+	brightnessLevel = input(inputPrompt)
+	validateInput(brightnessLevel)
+	brightnessLevel = int(brightnessLevel)
+	brightnessLevel = (brightnessLevel * 6) / 100
+	enhancementOfImage = ImageEnhance.Brightness(image)
+	enhancementOfImage.enhance(brightnessLevel).show()
+
+def contrast():
+	contrastLevel = input(inputPrompt)
+	validateInput(contrastLevel)
+	contrastLevel = int(contrastLevel)
+	contrastLevel = (contrastLevel * 6) / 100
+	enhancementOfImage = ImageEnhance.Contrast(image)
+	enhancementOfImage.enhance(contrastLevel).show()
+
+def validateInput(inputNumber):
+	try:
+		inputNumber = int(inputNumber)
+		if inputNumber == 0 or inputNumber > 100:
+			print("Enter a number in mentioned range only.")
+			exit()
+	except ValueError:
+		print("Enter a valid input.")
+
+
+functionList = [convertColorImageToGrayScale, brightness, contrast, blur]
+
+while True:
+	print(menu)
+	try:
+		userChoice = input("Enter you choice: ")
+		userChoice = int(userChoice)
+		if userChoice != 5:
+			functionList[userChoice - 1]()
+		else:
+			print("Do you really want to exit? ")
+			exitChoice = input("Type(y or n): ")
+			if exitChoice == 'y':
+				exit()
+			
+	except Exception:
+		print("Invalid Choice")
+	print("-" * 20)
